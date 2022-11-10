ec15a0af48f2358bd5d0437fd0bb48b1ad2e81cf
susindranjayaprakash <susindran.dreamgoal@gmail.com>
2022-11-10 23:36:05 +0530

    testing01

---
+++ src/test/java/mavennew01/NewTest01.java
@@ -0,0 +1,9 @@
+package mavennew01;
+
+import org.testng.annotations.Test;
+
+public class NewTest01 {
+  @Test
+  public void f() {
+  }
+}
---
+++ src/test/java/mavennew01/NewTest02.java
@@ -0,0 +1,10 @@
+package mavennew01;
+
+import org.testng.annotations.Test;
+
+public class NewTest02 {
+  @Test
+  public void f() {
+	  System.out.println("newtest01");
+  }
+}
